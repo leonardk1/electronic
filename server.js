f80b9c629175f69587367de65e7db2266ebaf454
Leonard Kakande <leonardk@ubuni.co.tz>
2025-01-31 17:20:44 +0300

    data response

--- server.js
+++ server.js
@@ -11,13 +11,28 @@ const handleMessage = (msg, rinfo) => {
     let receivedData;
     try {
         receivedData = JSON.parse(msg);
+        const responseObject = { response: "success", received_data: receivedData };
+        sendResponse(responseObject, rinfo);
     } catch (err) {
         console.error('Invalid JSON received:', msg);
+        const responseObject = { response: "failed", received_data: receivedData };
+        sendResponse(responseObject, rinfo);
         return;
     }
 
     console.log(receivedData)
 
+};
+
+// Function to send a response
+const sendResponse = (response, rinfo) => {
+    server.send(JSON.stringify(response), rinfo.port, rinfo.address, (err) => {
+        if (err) {
+            console.error('Error sending response:', err);
+        } else {
+            console.log('Response sent:', JSON.stringify(response));
+        }
+    });
 };
 
 
